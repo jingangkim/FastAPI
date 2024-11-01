784fdedc2605d853d3de152dc3d8aa5dab446078
jingangkim <seeu4534@gmail.com>
2024-11-01 10:53:06 +0900

    'httpx add'

--- app/routers/article_router.py
+++ app/routers/article_router.py
@@ -3,7 +3,8 @@ from fastapi import APIRouter
 from app.dtos.article_and_comments_response import ArticleAndCommentsResponse
 from app.services.article_service import service_get_article_and_comments
 
-router = APIRouter(prefix='/v1/articles', tags=['articles'], redirect_slashes=False)
+router = APIRouter(prefix="/v1/articles", tags=["articles"], redirect_slashes=False)
+
 
 @routers.get("/{article_id}", response_model=ArticleAndCommentsResponse)
 async def get_article_and_comments(article_id: int) -> ArticleAndCommentsResponse:
